242a783b6c0532b69cd57af33e99a2c9a8c27166
lolipuru <banstellazz@gmail.com>
2023-01-25 01:57:09 +0700

    sm8250-common: add pdx204 into sim init
    sad to see this device is really forgetten, a minor helpful for someone who really own this device
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- rootdir/bin/init.qcom.msim.sh
+++ rootdir/bin/init.qcom.msim.sh
@@ -3,7 +3,7 @@
 model=`sed -e '/[Mm][Oo][Dd][Ee][Ll]: /!d' -e 's/^.*[Mm][Oo][Dd][Ee][Ll]: \([A-Za-z0-9-]*\).*$/\1/' /dev/block/bootdevice/by-name/LTALabel` 2> /dev/null
 
 case "$model" in
-    "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
+    "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AQ52" | "XQ-AQ62" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
         setprop vendor.radio.multisim.config dsds;;
     * )
         setprop vendor.radio.multisim.config ss;;
